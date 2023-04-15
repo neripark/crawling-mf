0fd0a1c0f1097b6e2c3ed32439d56cff0ae91a2c
neripark <neripark@gmail.com>
2023-04-15 19:31:04 +0900

    init generate date label function

---
+++ src/generateDateLabel.ts
@@ -0,0 +1,15 @@
+import { format, subMonths, startOfMonth, endOfMonth } from "date-fns";
+
+export const generateDateLabelOnMf = () => {
+  const now = new Date();
+  const lastMonth = subMonths(now, 1);
+  const firstDayOfLastMonth = startOfMonth(lastMonth);
+  const lastDayOfLastMonth = endOfMonth(lastMonth);
+
+  const dateString = `${format(firstDayOfLastMonth, "yyyy/MM/dd")} - ${format(
+    lastDayOfLastMonth,
+    "yyyy/MM/dd"
+  )}`;
+
+  return dateString;
+};

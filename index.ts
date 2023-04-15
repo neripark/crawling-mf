24313fa387d2ab05a6e0ba3edeb3a03f8eb85701
neripark <neripark@gmail.com>
2023-04-15 13:23:13 +0900

    init index.ts and read env by dotenv

---
+++ index.ts
@@ -0,0 +1,31 @@
+import puppeteer from 'puppeteer';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+(async () => {
+  if (process.env.URL === undefined || process.env.SELECTOR === undefined) {
+    throw new Error("必要な環境変数がありません。");
+  }
+
+  console.log(process.env.URL);
+  console.log(process.env.SELECTOR);
+
+  const url =process.env.URL; 
+  const selector =process.env.SELECTOR; 
+
+  // Puppeteer の起動
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // 特定の URL にアクセス
+  await page.goto(url);
+
+  // DOM からテキストを取得
+  const element = await page.$(selector);
+  const text = await page.evaluate(element => element!.textContent, element);
+  console.log(text); // テキストの表示
+
+  // Puppeteer の終了
+  await browser.close();
+})();

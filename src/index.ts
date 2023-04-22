def36aea2e0265ab4940e6839d7fd815e81cefb2
neripark <neripark@gmail.com>
2023-04-22 22:43:57 +0900

    improve

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,6 @@ dotenv.config();
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
   );
   console.log("[start] visit site...");
-  console.log(page.content());
   await page.goto("https://moneyforward.com/cf#daily_info");
   await page.click('a[href^="/sign_in/email"]');
 
@@ -39,7 +38,7 @@ dotenv.config();
   await page.waitForSelector("input[type='email']");
   await page.type("input[type='email']", process.env.LOGIN_EMAIL);
   await page.click("input.submitBtn.homeDomain[type=submit]");
-  await page.waitForNavigation();
+  await page.waitForNavigation({ waitUntil: "networkidle0" });
 
   // 7. パスワードのインプットボックスにパスワードを入力して次へ
   console.log("[start] input password...");
@@ -52,13 +51,14 @@ dotenv.config();
 
   // 10. 表示を先月に切り替える
   console.log("[start] change view to last month...");
-  await page.click(
-    "button.btn.fc-button.fc-button-prev.spec-fc-button-click-attached"
-  );
+  const SELECTOR_BUTTON =
+    "button.btn.fc-button.fc-button-prev.spec-fc-button-click-attached";
+  await page.waitForSelector(SELECTOR_BUTTON);
+  await page.click(SELECTOR_BUTTON);
 
   // 11. 特定の要素のテキストが `前月1日〜前月末日`となっていることを確認する
-  await page.waitForSelector(".fc-header-title.in-out-header-title h2");
-
+  const SELECTOR_DATE_LABEL = ".fc-header-title.in-out-header-title h2";
+  await page.waitForSelector(SELECTOR_DATE_LABEL);
   // note: ブラウザ側に渡される関数だが、使用する変数は連れて行ってはくれないので、第3引数に記述して渡す必要がある
   console.log("[start] wait for display last month...");
   const pastMonthLabel = await page.evaluate(
@@ -67,9 +67,7 @@ dotenv.config();
   );
   await page.waitForFunction(
     (expectedLabel) => {
-      const element = document.querySelector(
-        ".fc-header-title.in-out-header-title h2"
-      );
+      const element = document.querySelector(SELECTOR_DATE_LABEL);
       if (element) {
         const text = element.textContent!.trim();
         return text === expectedLabel;
@@ -82,11 +80,12 @@ dotenv.config();
 
   // 12. 特定のtable要素が表示されるのを待つ
   console.log("[start] wait for display target table...");
-  await page.waitForSelector("table#cf-detail-table");
+  const SELECTOR_TABLE = "table#cf-detail-table";
+  await page.waitForSelector(SELECTOR_TABLE);
 
   const serializedTable = await page.evaluate(() => {
     // todo: MfTableクラス側に寄せる
-    const table = document.querySelector("table#cf-detail-table");
+    const table = document.querySelector(SELECTOR_TABLE);
     if (table === null) throw new Error("対象テーブルが見つかりませんでした。");
     // note: DOMのままではNode.jsに渡せないためいったん文字列にする
     return table.outerHTML;

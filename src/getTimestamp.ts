b0a2be1e914d4829fc8620c2fe9018e2940dacad
neripark <neripark@gmail.com>
2023-04-22 19:04:25 +0900

    improve timestamp

--- src/getTimestamp.ts
+++ src/getTimestamp.ts
@@ -8,6 +8,6 @@ export const getTimestamp = () => {
     .padStart(2, "0")}_${date.getMinutes().toString().padStart(2, "0")}_${date
     .getSeconds()
     .toString()
-    .padStart(2, "0")}`;
+    .padStart(2, "0")}_${date.getMilliseconds().toString().padStart(3, "0")}`;
   return timestamp;
 };

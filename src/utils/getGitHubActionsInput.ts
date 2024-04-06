9099c2ae2f515bf80760acd68111204eb03666c6
neripark <neripark@gmail.com>
2024-04-06 13:32:22 +0900

    init function for getting input from github actions

---
+++ src/utils/getGitHubActionsInput.ts
@@ -0,0 +1,14 @@
+import {getInput, setFailed } from '@actions/core'
+
+export const getGitHubActionsInput = async (inputName: string): Promise<string> => {
+  try {
+    const input = getInput(inputName);
+    console.log(`[INFO] input from GitHub Actions '${inputName}': `, input);
+    return input;
+  } catch (error) {
+    if (error instanceof Error) {
+      setFailed(error)
+    }
+  }
+  throw new Error("not reach here");
+}

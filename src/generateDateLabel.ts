989735e5d3ec31fc1c897ba12520ea440a050e41
neripark <neripark@gmail.com>
2024-04-06 18:31:15 +0900

    improve generate date label function

--- src/generateDateLabel.ts
+++ src/generateDateLabel.ts
@@ -1,13 +1,13 @@
 import { format, subMonths, startOfMonth, endOfMonth } from "date-fns";
 
-export const generateDateLabelOnMf = () => {
+export const generateDateLabelOnMf = (numberToBack?: number) => {
   const now = new Date();
-  const lastMonth = subMonths(now, 1);
-  const firstDayOfLastMonth = startOfMonth(lastMonth);
-  const lastDayOfLastMonth = endOfMonth(lastMonth);
+  const targetMonth = subMonths(now, numberToBack ?? 1); // default: 1 = 先月
+  const firstDayOfTargetMonth = startOfMonth(targetMonth);
+  const lastDayOfTargetMonth = endOfMonth(targetMonth);
 
-  const dateString = `${format(firstDayOfLastMonth, "yyyy/MM/dd")} - ${format(
-    lastDayOfLastMonth,
+  const dateString = `${format(firstDayOfTargetMonth, "yyyy/MM/dd")} - ${format(
+    lastDayOfTargetMonth,
     "yyyy/MM/dd",
   )}`;
 

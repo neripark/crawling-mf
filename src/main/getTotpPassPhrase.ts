30a48e3c136f79e5521ecea37f2686eb26061a92
neripark <neripark@gmail.com>
2025-02-08 23:40:31 +0900

    add comment

--- src/main/getTotpPassPhrase.ts
+++ src/main/getTotpPassPhrase.ts
@@ -1,6 +1,7 @@
 import { TOTP } from "totp-generator";
 
 export const getTotpPassPhrase = () => {
+  // todo: 型注釈を追加してundefinedを考慮しないで済むようにする
   if (!process.env.LOGIN_TOTP_SECRET) {
     throw new Error("LOGIN_TOTP_SECRET is not defined");
   }

2dd7abfe666bdc3f850d9093e2b29d31e17aaaae
neripark <neripark@gmail.com>
2025-03-16 13:43:42 +0900

    draft

--- src/repositories/postToLineNotify.ts
+++ src/repositories/postToLineNotify.ts
@@ -1,12 +1,25 @@
 export const notifyToLine = async (message: string) => {
-  return fetch("https://notify-api.line.me/api/notify", {
+  return fetch("https://api.line.me/v2/bot/message/push", {
     method: "POST",
     headers: {
-      Authorization: `Bearer ${process.env.LINE_NOTIFY_TOKEN}`,
-      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Bearer ${process.env.LINE_MESSAGING_API_CHANNEL_ACCESS_TOKEN}`,
+      "Content-Type": "application/json",
     },
     body: new URLSearchParams({
       message,
     }).toString(),
   });
 };
+
+// export const notifyToLine = async (message: string) => {
+//   return fetch("https://notify-api.line.me/api/notify", {
+//     method: "POST",
+//     headers: {
+//       Authorization: `Bearer ${process.env.LINE_NOTIFY_TOKEN}`,
+//       "Content-Type": "application/x-www-form-urlencoded",
+//     },
+//     body: new URLSearchParams({
+//       message,
+//     }).toString(),
+//   });
+// };

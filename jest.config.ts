0f29ba03d0b898edcbc3a42404da2fc7a89ad243
neripark <neripark@gmail.com>
2025-04-26 13:01:46 +0900

    improve

--- jest.config.ts
+++ jest.config.ts
@@ -4,10 +4,6 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  testMatch: ['<rootDir>/src/**/?(*.)test.ts'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
 };
 

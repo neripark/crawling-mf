5be60635456e8e464e85cc7aef8337312897baa5
neripark <neripark@gmail.com>
2025-04-26 13:06:38 +0900

    run format

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from 'jest';
+import type { Config } from "jest";
 
 const config: Config = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  moduleFileExtensions: ["ts", "js", "json", "node"],
 };
 
 export default config;
